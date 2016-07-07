51554f8213902737927d4ddf0568b04ac7a71a52
INJAE <8687lee@naver.com>
2016-07-07 23:19:07 +0900

    Second Commit

---
+++ Cryptonite/src/Server/Server_Main.java
@@ -0,0 +1,11 @@
+package Server;
+
+public class Server_Main 
+{
+	public static void main(String[] args)
+	{
+		// TODO 자동 생성된 메소드 스텁
+
+	}
+
+}
---
+++ Cryptonite/src/Server/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author user
+ *
+ */
+package Server;
