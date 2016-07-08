0e37728e95143e7df583fe53e055bbf2df790272
INJAE <8687lee@naver.com>
2016-07-08 15:46:35 +0900

    UI_Listener Update

--- Cryptonite/src/Function/UI_Listener.java
+++ Cryptonite/src/Function/UI_Listener.java
@@ -1,25 +1,36 @@
-package Client;
+package Function;
 
-import java.util.Vector;
+import java.util.HashMap;
 
 public class UI_Listener 
 {
-	private Vector<Object> UI_List;
+	private HashMap<String,Object> UI_List;
 	private static UI_Listener Listener;
 	
 	private UI_Listener()
 	{
-		
+		UI_List = new HashMap<String,Object>();
 	}
 	
 	public UI_Listener getInstance()
 	{
 		if(Listener == null)
 		{
-			Listener = new UI_Listener();
-			UI_List = new Vector<Object>();
+			Listener = new UI_Listener();	
 		}
 		
 		return Listener;
+	}
+	
+	public Object getUI(String name)
+	{
+		Object UI = UI_List.get(name);
+		if(UI == null) { return null; }
+		else		   { return UI; }
+	}
+	
+	public void setUI(String name,Object UI)
+	{
+		UI_List.put(name, UI);
 	}
 }
