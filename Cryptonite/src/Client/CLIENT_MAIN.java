08108fadc64d4fdfc3e018a16419edf06c88a636
Youn <Youn@heeseung-pc>
2016-07-08 12:57:57 +0900

    first commit

--- Cryptonite/src/Client/CLIENT_MAIN.java
+++ Cryptonite/src/Client/CLIENT_MAIN.java
@@ -8,8 +8,6 @@ public class CLIENT_MAIN
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World~!");
-		System.out.println("new add");
-		System.out.println("dev branch commit test");
+		
 	}
 }
