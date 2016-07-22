a783d6d9ddb52b78a6c3771f3434488302701c4b
Youn <Youn@heeseung-pc>
2016-07-22 17:32:23 +0900

    NIO_FileManager finish

--- Cryptonite/src/Client/Client_AutoBackup.java
+++ Cryptonite/src/Client/Client_AutoBackup.java
@@ -1,6 +1,34 @@
 package Client;
 
-public class Client_AutoBackup 
-{
+import java.net.*;
+import java.nio.*;
+import java.nio.file.*;
+import java.nio.channels.*;
+import java.io.*;
+import java.util.*;
+import Function.NIO_FileManager;
 
+public class Client_AutoBackup extends Thread
+{
+	// SocketChannel
+	private SocketChannel socketChannel = null;
+	
+	// 
+	
+	// Constructors
+	
+	// Methods
+	public synchronized void run()
+	{
+		try 
+		{
+			socketChannel = SocketChannel.open();
+			socketChannel.configureBlocking(true);
+			System.out.println("Complete Server Connecting !!");
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+	}
 }
