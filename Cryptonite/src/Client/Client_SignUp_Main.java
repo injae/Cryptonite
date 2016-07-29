c4a0e1d3acf1c820f8c36b77bf998975734f8eed
김민정 <@samsung>
2016-07-29 18:59:10 +0900

    Sign_up

--- Cryptonite/src/Client/Client_SignUp_Main.java
+++ Cryptonite/src/Client/Client_SignUp_Main.java
@@ -6,10 +6,12 @@ import Server.Server_DataBase;
 
 public class Client_SignUp_Main {
 	public static void main(String[ ] args)throws InterruptedException, IOException{
-		Server_DataBase db;
+		/*TEST
+		 * 
+		 * Server_DataBase db;
 		db=Server_DataBase.getInstance();
-		db.Init_DB("com.mysql.jdbc.Driver", "jdbc.mysql://127.0.0.1:3306/"+"cryptonite", "root", "yangmalalice3349!");
+		db.Init_DB("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1:3306/"+"cryptonite", "root", "yangmalalice3349!");
 		db.connect();
-		db.Update("INSERT INTO USER_INFORMATION VALUES('MJ','alfzl1','yangmalalice3349','alfzl1@naver.com')");
+		db.Update("INSERT INTO USER_INFORMATION_TEST VALUES('A','A','A','A')");*/
 	}
 }
