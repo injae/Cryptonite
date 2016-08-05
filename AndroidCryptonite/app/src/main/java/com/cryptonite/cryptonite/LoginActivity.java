0aa6198d5237a762df45e472abe8abf9e77047a4
wns25678 <wns25678@naver.com>
2016-08-05 12:47:15 +0900

    Login change

--- AndroidCryptonite/app/src/main/java/com/cryptonite/cryptonite/LoginActivity.java
+++ AndroidCryptonite/app/src/main/java/com/cryptonite/cryptonite/LoginActivity.java
@@ -45,13 +45,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
+
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -327,13 +321,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 return false;
             }
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mid)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
 
             // TODO: register the new account here.
             return true;
