c4c8ac0f673359f508db88a224048881468984e2
김민정 <@samsung>
2016-08-10 22:51:11 +0900

    logo.png & sign_up,login

--- Cryptonite/src/Client/Client_Main.java
+++ Cryptonite/src/Client/Client_Main.java
@@ -12,14 +12,14 @@ public class Client_Main
 {
 	public static void main(String[] args) throws InterruptedException, IOException
 	{		
-		Client_Server_Connector csc = Client_Server_Connector.getInstance();
+	/*	Client_Server_Connector csc = Client_Server_Connector.getInstance();
 			
 		Client_AutoBackup cab = new Client_AutoBackup();
-		cab.start();
+		cab.start();*/
 		
 		/*Client_FileShare_Send cfs = new Client_FileShare_Send();
 		cfs.start();*/
-		
+		Client_Login cl=new Client_Login();
 		/*Client_FileShare_Receive cfr = new Client_FileShare_Receive();
 		cfr.receiveFiles();*/
 		
