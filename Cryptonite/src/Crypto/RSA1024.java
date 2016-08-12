88724f58998996b594c66b6611830c6635af1db0
nbios <nbiosupr@gmail.com>
2016-08-12 15:16:30 +0900

    Seperate Crypto System

--- Cryptonite/src/Crypto/RSA1024.java
+++ Cryptonite/src/Crypto/RSA1024.java
@@ -1,5 +1,38 @@
 package Crypto;
 
-public class RSA1024 {
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
 
+public class RSA1024 extends Crypto{
+	private static final String algorithm = "RSA"; 
+	private static final String transformation = algorithm + "/ECB/PKCS5Padding";
+	
+	//Constructor
+	public RSA1024() {
+		super();
+	}
+	public RSA1024(SecretKey _key) {
+		super(_key);
+	}
+	public RSA1024(int _mode) {
+		super(_mode);
+	}
+	public RSA1024(SecretKey _key, int _mode) {
+		super(_key, _mode);
+	}
+	
+	public boolean push (byte[] target){
+		try {
+			Cipher cipher = Cipher.getInstance(transformation);
+			cipher.init(_mode, _key);
+			_resultData = cipher.doFinal(target);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}		
+	}
+	public byte[] pop(){
+		return _resultData;
+	}
 }
--- Cryptonite/src/Crypto/userKeyGenerator.java
+++ Cryptonite/src/Crypto/userKeyGenerator.java
@@ -1,24 +1,28 @@
-package Function;
+package Crypto;
 
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.SecureRandom;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
-public class KeyGenerator {
+public class userKeyGenerator {
 	private byte[] _salt = null;
 	private SecretKey _keyData = null;
 	private int _iterationCount = 0;
 	private SecureRandom _random = null;
 
 	//Constructor
-	public KeyGenerator()
+	public userKeyGenerator()
 	{
 		init();
 	}
 	
 	//Getter
 	public SecretKey getAesKey() { return _keyData; }
+	public byte[] getAesKeyBytes() { return _keyData.getEncoded(); }
 	public byte[] getSalt() { return _salt; }
 	public int getIterationCount() { return _iterationCount; }
 	
@@ -46,8 +50,6 @@ public class KeyGenerator {
 		int iterationCount = 1000; //Test Integer
 		return iterationCount;
 	}
+	
 }
 
-/* 鍮꾨��궎�� �넄�듃媛믨낵 諛섎났移댁슫�듃 媛믪쓣 �룞�떆�뿉 �깮�꽦�븿
- * �븳 �븘�씠�뵒�뿉 遺��뿬�븷 �븣留덈떎 init()�쓣 �샇異쒗빐以섏빞�븿
- */
