d76c899b79097810a009a52460d6ce80d801708e
Youn <Youn@heeseung-pc>
2016-08-19 14:04:22 +0900

    PacketRule UPDATE

--- Cryptonite/src/Client/Client_File_ListReceiver.java
+++ Cryptonite/src/Client/Client_File_ListReceiver.java
@@ -1,6 +1,31 @@
 package Client;
 
-public class Client_File_ListReceiver 
-{
+import Function.PacketRule;
 
+public class Client_File_ListReceiver implements PacketRule
+{
+	// Instance
+	private byte _mod;
+	
+	// Constructors
+	
+	// Methods
+	public void click(byte mod, String gpName)
+	{
+		byte[] event = new byte[1024];
+		event[0] = FILE_LIST_REQUEST;
+		event[1] = mod;
+		if(mod == 1)
+		{
+			byte[] temp = gpName.getBytes();
+			for(int i = 0; i < gpName.getBytes().length; i++)
+			{
+				event[i + 2] = temp[i];
+			}
+		}
+		else if(mod == 2)
+		{
+			
+		}
+	}
 }
