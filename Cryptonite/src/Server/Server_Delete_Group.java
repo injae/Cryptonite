a359a7ffd92928469dcad6398ebfb23b3765e595
Youn <Youn@DESKTOP-OIIE9KD>
2016-08-22 17:26:06 +0900

    DELETE GROUP CLASS EMERGE

---
+++ Cryptonite/src/Server/Server_Delete_Group.java
@@ -0,0 +1,24 @@
+package Server;
+
+import java.io.IOException;
+
+public class Server_Delete_Group extends Server_Funtion {
+
+	public Server_Delete_Group(Server_Client_Activity activity) 
+	{
+		super(activity);
+	}
+
+	@Override
+	public void Checker(byte[] packet) 
+	{
+		
+	}
+
+	@Override
+	public void running(int count) throws IOException 
+	{
+
+	}
+
+}
