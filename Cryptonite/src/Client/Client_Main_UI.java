48213279adcfb31c664ae2f98fdb55ef3da7da09
Youn <Youn@DESKTOP-OIIE9KD>
2016-08-27 16:29:41 +0900

    UI RECOVERY

--- Cryptonite/src/Client/Client_Main_UI.java
+++ Cryptonite/src/Client/Client_Main_UI.java
@@ -7,7 +7,11 @@ import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -26,18 +30,20 @@ public class Client_Main_UI extends JFrame
 	private ArrayList<String> _gpName;
 	private String _name;
 	private String _usCode;
+	private String _address;
+	private Client_FolderSelector cfs = null;
 	
 	private BufferedImage img = null;
 	private BufferedImage img2=null;
 	
 	private JButton Sendbt;
 	private JButton Receivebt;
-	private JButton Encryptbt;
+	private JButton ProtectedFolderbt;
 	private JButton Cloudbt;
 	private JButton Indivbt;
 	private JButton Groupbt;
 	private JButton Developerbt;
-	private JButton Settingbt;
+	private JButton ProtectedFolderOpenbt;
 	private JButton tab;
 	
 	private JButton Create;
@@ -98,7 +104,7 @@ public class Client_Main_UI extends JFrame
         layeredPane.add(Indivbt);
  		layeredPane.add(Groupbt);
         layeredPane.add(Developerbt);
-        layeredPane.add(Settingbt);
+        layeredPane.add(ProtectedFolderOpenbt);
         
  	    layeredPane.add(Create);
  	    layeredPane.add(Participate);
@@ -114,11 +120,11 @@ public class Client_Main_UI extends JFrame
 		 layeredPane.add(Indivbt);
 		 layeredPane.add(Groupbt);
 		 layeredPane.add(Developerbt);
-		 layeredPane.add(Settingbt);
+		 layeredPane.add(ProtectedFolderOpenbt);
 		 
 		 layeredPane.add(Sendbt);
 		 layeredPane.add(Receivebt);
-		 layeredPane.add(Encryptbt); 
+		 layeredPane.add(ProtectedFolderbt); 
 		 layeredPane.add(Cloudbt);
 		 
 		 layeredPane.add(panel);
@@ -163,15 +169,32 @@ public class Client_Main_UI extends JFrame
         Developerbt.setContentAreaFilled(false);
         
         
-        Settingbt = new JButton(new ImageIcon("img/Settingbt.png"));
-        Settingbt.setPressedIcon(new ImageIcon("img/Settinghbt.png"));
-        Settingbt.setBounds(707, 4, 60, 60);
-        Settingbt.setFocusPainted(false);
-        Settingbt.setContentAreaFilled(false);
-        Settingbt.setBorderPainted(false);
-        
-        //--------------------
-        
+        ProtectedFolderOpenbt = new JButton(new ImageIcon("img/protectedFolderOpen.png"));
+        ProtectedFolderOpenbt.setPressedIcon(new ImageIcon("img/protectedFolderOpenR.png"));
+        ProtectedFolderOpenbt.setBounds(740, 20, 30, 30);
+        ProtectedFolderOpenbt.setFocusPainted(false);
+        ProtectedFolderOpenbt.setContentAreaFilled(false);
+        ProtectedFolderOpenbt.setBorderPainted(false);
+        ProtectedFolderOpenbt.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+		        try 
+		        {
+		        FileReader fr = new FileReader(new File("Cryptonite_Client/log/protectedlog.ser"));
+		        BufferedReader br = new BufferedReader(fr);
+		        String address = br.readLine();
+		        Runtime rt = Runtime.getRuntime();
+		        rt.exec("explorer.exe " + address);
+		        } 
+		        catch (FileNotFoundException e1) 
+		        {
+		        	e1.printStackTrace();
+		        } 
+		        catch (IOException e1) 
+		        {
+		        	e1.printStackTrace();
+		        }
+        	}
+        });
         
         Sendbt = new JButton(new ImageIcon("img/Filesend.png"));		
         Sendbt.setPressedIcon(new ImageIcon("img/Filesendh.png"));
@@ -197,12 +220,42 @@ public class Client_Main_UI extends JFrame
 			}
 		});
         
-        Encryptbt = new JButton(new ImageIcon("img/protectedfolder.png"));
-        Encryptbt.setPressedIcon(new ImageIcon("img/protectedfolderh.png"));
-        Encryptbt.setFocusPainted(false);
-        Encryptbt.setContentAreaFilled(false);
-        Encryptbt.setBorderPainted(false);
-        Encryptbt.setBounds(1, 245, 470, 190);
+        ProtectedFolderbt = new JButton(new ImageIcon("img/protectedfolder.png"));
+        ProtectedFolderbt.setPressedIcon(new ImageIcon("img/protectedfolderh.png"));
+        ProtectedFolderbt.setFocusPainted(false);
+        ProtectedFolderbt.setContentAreaFilled(false);
+        ProtectedFolderbt.setBorderPainted(false);
+        ProtectedFolderbt.setBounds(1, 245, 470, 190);
+        ProtectedFolderbt.addActionListener(new ActionListener() {
+        	 public void actionPerformed(ActionEvent e) {
+        	 cfs = new Client_FolderSelector();
+        	 cfs.folderSelectorON();
+        	 while(!cfs.getSelectionEnd())
+        	 {
+        	 	try 
+        	 	{
+        	 		Thread.sleep(1);
+        	 	} 
+        	 	catch (InterruptedException e1) 
+        	 	{
+        	 		e1.printStackTrace();
+        	 	}
+        	 }	
+        	 _address = cfs.getSelectedPath();
+        	 File save = new File("Cryptonite_Client/log/protectedlog.ser");
+        	 FileWriter fw;
+        	 try {
+        	 	fw = new FileWriter(save);
+        	 	fw.write(_address);
+        	 	fw.close();
+        	 } 
+        	 catch (IOException e1) 
+        	 {
+        	 	e1.printStackTrace();
+        	 }
+        	 new Client_FolderScan().start();		
+        	 }
+        	 });
         
         Cloudbt = new JButton(new ImageIcon("img/Cloud.png"));
         Cloudbt.setRolloverIcon(new ImageIcon("img/Cloudh.png"));
@@ -236,9 +289,41 @@ public class Client_Main_UI extends JFrame
  	    Participate.setContentAreaFilled(false);
  	    Participate.addActionListener(new ActionListener() {
  	       	public void actionPerformed(ActionEvent arg0) {
- 	       		Scanner sc = new Scanner(System.in);
+ 	       	while(true)
+    		{
+    			System.out.println("하고싶은 모드를 입력하세요 (1. 사람검색, 2. 사람초대, 3. 그룹삭제, 4. 그룹탈퇴) ");
+    			System.out.print("입력 : ");
+    			Scanner scanner = new Scanner(System.in);
+    			int choice = scanner.nextInt();
+    			
+    			switch(choice)
+    			{
+    			case 1:
+    				System.out.print("아이디를 입력하세요 : ");
+    				new Client_Group_Search().search();
+    				break;
+    			case 2:
+    				System.out.print("그룹코드를 작성하세요 : ");
+    				String gpCode = scanner.nextLine();
+    				new Client_Group_Invite().running(gpCode);
+    				break;
+    			case 3:
+    				System.out.print("그룹코드를 작성하세요 : ");
+    				String gpCode2 = scanner.nextLine();
+    				new Client_Delete_Group().deleteGroup(gpCode2);
+    				break;
+    			case 4:
+    				System.out.print("그룹코드를 작성하세요 : ");
+    				String gpCode3 = scanner.nextLine();
+    				new Client_Group_Withdrawal().running(gpCode3);
+    				break;
+    				default:
+    					break;
+    			}
+    		}
+ 	       		/*Scanner sc = new Scanner(System.in);
  	       		String code = sc.nextLine();
- 	       		new Client_Group_Withdrawal().running(code);
+ 	       		new Client_Group_Withdrawal().running(code);*/
  	       	}
  	    });
 		//----------------------------------------------------------------------indivial
