35a7609b05737a1f7a1320b839bd81f0166e89ac
전용범 <@192.168.56.1>
2016-11-09 03:27:27 +0900

    Android PBKDF2, fix map

--- AndroidCryptonite/app/src/main/java/Function/Client_Get_Group_Key.java
+++ AndroidCryptonite/app/src/main/java/Function/Client_Get_Group_Key.java
@@ -17,7 +17,7 @@ public class Client_Get_Group_Key implements PacketRule
     {
         Client_Server_Connector csc = Client_Server_Connector.getInstance();
         KeyReposit reposit = KeyReposit.getInstance();
-        Crypto crypto = new Crypto(Crypto_Factory.create("AES256", Cipher.DECRYPT_MODE, reposit.get_aesKey()));
+        //Crypto crypto = new Crypto(Crypto_Factory.create("AES256", Cipher.DECRYPT_MODE, reposit.get_aesKey()));
 
         byte[] event = new byte[1024];
         event[0] = GET_GROUP_KEY;
@@ -27,7 +27,7 @@ public class Client_Get_Group_Key implements PacketRule
         try
         {
             csc.send.setPacket(event).write();
-            GpKey = new SecretKeySpec(crypto.endecription(csc.receive.setAllocate(32).read().getByte()), "AES");
+            GpKey = new SecretKeySpec(csc.receive.setAllocate(32).read().getByte(), "AES");
         } catch (IOException e) {
             // TODO 자동 생성된 catch 블록
             e.printStackTrace();
